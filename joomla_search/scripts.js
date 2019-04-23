b81f6af815063804d00490f01047cb9b4977475e
Jmackas <jamesrossmackay@gmail.com>
2019-04-23 20:09:11 +0800

    Targetting the search page

--- joomla_search/scripts.js
+++ joomla_search/scripts.js
@@ -2,10 +2,16 @@ extensionInterface();
 
 function extensionInterface() {
     // Fetching function page
-    let searchPageCheck = document.querySelectorAll(".body.site.com_search.view-search");
+    let searchPageCheck = document.getElementById("searchForm");
 
     // Check if user is on grade page
     if (searchPageCheck !== null) {
+        initiateInterface();
+    }
+
+    // Get the project in an instance that is safe
+    function initiateInterface() {
         console.log("hello");
+        //let searchPageCheck = document.querySelectorAll(".body.site.com_search.view-search");
     }
 }
