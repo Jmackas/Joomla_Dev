f42e058a825d41289e1699522e5e14804f4e216d
Jmackas <jamesrossmackay@gmail.com>
2019-04-23 21:14:28 +0800

    Multiple row generation completed

--- joomla_search/scripts.js
+++ joomla_search/scripts.js
@@ -26,17 +26,31 @@ function extensionInterface() {
         // Add initial table
         function addTable() {
             // Determine the amount of columns
-            let amountOfColumns = document.querySelector('.badge-info').innerHTML;
-            amountOfColumns = parseInt(amountOfColumns);
+            let amountOfRows = document.querySelector('.badge-info').innerHTML;
+            amountOfRows = parseInt(amountOfRows);
 
             let tableCreate = document.querySelector('.search-results');
 
             // Create the bottom of the table
-            tableCreate.insertAdjacentHTML('afterbegin', '<table id="generatedTable"><tbody><tr><td id="titleRes">x</td><td id="unitOutlineRes">x</td><td id="textRes">x</td></tr></tbody></table>')
+            tableCreate.insertAdjacentHTML('afterbegin', '<table id="generatedTable"><tbody><tr><td id="titleRes"></td><td id="unitOutlineRes"></td><td id="textRes"></td></tr></tbody></table>')
 
-            document.getElementById("generatedTable").insertRow();
-        
+            tableRowGenerator();
 
+            function tableRowGenerator() {
+                // Identify table
+                var table = document.getElementById("generatedTable");
+
+                //Generate rows, and assign ID's
+                for (let i = 1; i < amountOfRows; i++) {
+                    var row = table.insertRow(0);
+                    var cell1 = row.insertCell(0);
+                    var cell2 = row.insertCell(1);
+                    var cell3 = row.insertCell(2);
+                    cell1.setAttribute('id', 'titleRes');
+                    cell2.setAttribute('id', 'unitOutlineRes');
+                    cell3.setAttribute('id', 'textRes');
+                }
+            }
         }
 
         // Add the titles to the table
