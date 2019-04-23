fa1a17a5acdb63f8ba802491d2286b405ec78fee
Jmackas <jamesrossmackay@gmail.com>
2019-04-23 20:04:00 +0800

    Initiate function when on the search page

---
+++ joomla_search/scripts.js
@@ -0,0 +1,11 @@
+extensionInterface();
+
+function extensionInterface() {
+    // Fetching function page
+    let searchPageCheck = document.querySelectorAll(".body.site.com_search.view-search");
+
+    // Check if user is on grade page
+    if (searchPageCheck !== null) {
+        console.log("hello");
+    }
+}
