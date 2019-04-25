f2b6dc65492fa8a6a48c0f1b33be3eb7c47d443e
Jmackas <jamesrossmackay@gmail.com>
2019-04-25 16:59:53 +0800

    Table content added

--- joomla_search/scripts.js
+++ joomla_search/scripts.js
@@ -21,6 +21,7 @@ function extensionInterface() {
     function resultsToTables() {
         // Invoke the relevant functions
         addTable();
+        addTitle()
 
         // Add initial table
         function addTable() {
@@ -31,13 +32,13 @@ function extensionInterface() {
             let tableCreate = document.querySelector('.search-results');
 
             // Create the bottom of the table
-            tableCreate.insertAdjacentHTML('afterbegin', '<table id="generatedTable"><tbody><tr><td id="titleRes"></td><td id="unitOutlineRes"></td><td id="textRes"></td></tr></tbody></table>')
+            tableCreate.insertAdjacentHTML('beforebegin', '<table id="generatedTable"><tbody><tr><td id="titleRes">Title</td><td id="unitOutlineRes">Unit Outline</td><td id="textRes">Description</td></tr></tbody></table>')
 
             tableRowGenerator();
 
             function tableRowGenerator() {
                 // Identify table
-                var table = document.getElementById("generatedTable");
+                let table = document.getElementById("generatedTable");
 
                 //Generate rows, and assign ID's
                 for (let i = 1; i < amountOfRows; i++) {
@@ -45,16 +46,18 @@ function extensionInterface() {
                     var cell1 = row.insertCell(0);
                     var cell2 = row.insertCell(1);
                     var cell3 = row.insertCell(2);
-                    cell1.setAttribute('id', 'titleRes');
-                    cell2.setAttribute('id', 'unitOutlineRes');
-                    cell3.setAttribute('id', 'textRes');
+                   // cell1.setAttribute('id', 'titleRes');
+                   // cell2.setAttribute('id', 'unitOutlineRes');
+                    //cell3.setAttribute('id', 'textRes');
                 }
             }
         }
 
         // Add the titles to the table
         function addTitle() {
-            let searchFirstCol = document.querySelectorAll("#generatedTable #titleRes");
+            let searchFirstCol = document.querySelectorAll("#generatedTable td:nth-of-type(1)");
+            
+    
 
         }
     }
