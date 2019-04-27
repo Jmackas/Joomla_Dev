71017dadbdf32fabb790098c875e20c665d31b31
Jmackas <jamesrossmackay@gmail.com>
2019-04-27 16:17:39 +0800

    Download functionality complete

--- joomla_search/scripts.js
+++ joomla_search/scripts.js
@@ -139,12 +139,63 @@ function extensionInterface() {
         // Ability to download the table of results
         function downloadTableResults() {
             // Select the generated table
-           let tableCreate = document.querySelector('#generatedTable');
+            let tableCreate = document.querySelector('#generatedTable');
 
             // Insert the button for downloading the button
             tableCreate.insertAdjacentHTML('beforebegin', '<button id="downloadTable">Download Results</button>')
 
-            
+            // Create the CSV attribute
+            function download_csv(csv, filename) {
+                let csvFile;
+                let downloadLink;
+
+                // CSV FILE
+                csvFile = new Blob([csv],{
+                    type: "text/csv"
+                });
+
+                // Download link
+                downloadLink = document.createElement("a");
+
+                // File name
+                downloadLink.download = filename;
+
+                // We have to create a link to the file
+                downloadLink.href = window.URL.createObjectURL(csvFile);
+
+                // Make sure that the link is not displayed
+                downloadLink.style.display = "none";
+
+                // Add the link to your DOM
+                document.body.appendChild(downloadLink);
+
+                // Lanzamos
+                downloadLink.click();
+            }
+
+            function export_table_to_csv(html, filename) {
+                var csv = [];
+                var rows = document.querySelectorAll("table tr");
+
+                for (var i = 0; i < rows.length; i++) {
+                    var row = []
+                      , cols = rows[i].querySelectorAll("td, th");
+
+                    for (var j = 0; j < cols.length; j++)
+                        row.push(cols[j].innerText);
+
+                    csv.push(row.join(","));
+                }
+
+                // Download CSV
+                download_csv(csv.join("\n"), filename);
+            }
+
+            document.querySelector("#downloadTable").addEventListener("click", function() {
+                var html = document.querySelector("table").outerHTML;
+                export_table_to_csv(html, "table.csv");
+            });
+
         }
 
         // Lazy way of loading styles pertinant to this script only
