7a0d53ad02a1e5e34a045581237526ae7c637be3
Jmackas <jamesrossmackay@gmail.com>
2019-04-28 13:32:33 +0800

    Moving a function to the start of the script execution

--- joomla_search/scripts.js
+++ joomla_search/scripts.js
@@ -150,16 +150,15 @@ function extensionInterface() {
 
             */
             // Select the generated table
-            let tableCreate = document.querySelector('#generatedTable');
-
-            // Insert HR
-            tableCreate.insertAdjacentHTML('beforebegin', '<hr>')
-
-            // Insert filter to have unique unit outlines
-            tableCreate.insertAdjacentHTML('beforebegin', '<label class="radio"><input type="radio" id="uniqueUO">Unique Unit Outlines Only</label><br>')
+            let tableCreate = document.querySelector('.phrases');
 
             // Insert the button for downloading the button
-            tableCreate.insertAdjacentHTML('beforebegin', '<button id="downloadTable" class="btn">Download Results</button><br><br>')
+            tableCreate.insertAdjacentHTML('afterend', '<button id="downloadTable" class="btn">Download Results</button><br><br>')
+
+            // Insert filter to have unique unit outlines
+            tableCreate.insertAdjacentHTML('afterend', '<label class="radio"><input type="radio" id="uniqueUO">Unique Unit Outlines Only</label><br>')
+            // Insert HR
+            tableCreate.insertAdjacentHTML('afterend', '<hr>')
 
         }
 
