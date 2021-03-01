ba8a572bb2acd82bfa38c18be2a580172a29060d
James Mackay <jamesrossmackay@gmail.com>
2021-03-01 20:56:00 +0800

    Update clickup.js

--- clickup.js
+++ clickup.js
@@ -6,6 +6,22 @@ document.addEventListener("keydown", function(e) {
 }, false);
 
 
-document.head.insertAdjacentHTML("beforeend", `<style>.cu-comment__body-container {height: 300px;overflow: auto;}</style>`)
+//document.head.insertAdjacentHTML("beforeend", `<style>.cu-comment__body-container {height: 300px;overflow: auto;}</style>`)
+
+let comment = document.querySelectorAll(".cu-comment__body-container");
+
+for (let i = 0; i < comment.length; i++) {
+    // Determine the height of all comments
+    let commentHeight = comment[i].offsetHeight;
+
+    let commentToIntHeight = parseInt(commentHeight);
+
+    if (commentToIntHeight > 400) {
+        console.log("greater than 400");
+        comment[i].style.height = "300px";
+        comment[i].style.overflow = "auto";
+    }
+
+}
 
 
